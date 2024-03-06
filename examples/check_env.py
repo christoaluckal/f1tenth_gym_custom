36f36f90b07cac617d492be53f965865874ab461
Christo Aluckal <christo12aluckal@gmail.com>
2024-03-05 23:44:21 -0500

    initial commit

---
+++ examples/check_env.py
@@ -0,0 +1,14 @@
+from stable_baselines3.common.env_checker import check_env
+import gym
+from gym.envs.registration import register
+
+map_config = {
+        'map_ext': '.png',
+        'map': '/home/caluckal/Developer/spring2024/thesis/f1tenth_gym/gym/f110_gym/unittest/maps/map_15_100_',
+        'waypoints': '/home/caluckal/Developer/spring2024/thesis/f1tenth_gym/gym/f110_gym/unittest/centerline/map_15_100_.csv'
+    }
+
+register('f110_gym:f110-cust-v0', entry_point='f110_gym.envs:F110_Cust_Env', max_episode_steps=1000)
+env = gym.make('f110_gym:f110-cust-v0',config=map_config, num_agents=1, timestep=0.01, integrator=1, classic=False)
+
+check_env(env)
