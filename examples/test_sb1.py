c938bfac6088da0df471c961219723a3cbdd0730
Christo Aluckal <christo12aluckal@gmail.com>
2024-03-08 14:22:57 -0500

    testing policy update on callbacks

--- examples/test_sb1.py
+++ examples/test_sb1.py
@@ -10,6 +10,82 @@ from numba import njit
 
 from pyglet.gl import GL_POINTS
 
+from stable_baselines3.common.callbacks import BaseCallback,CheckpointCallback
+
+
+class CustomCallback(BaseCallback):
+    """
+    A custom callback that derives from ``BaseCallback``.
+
+    :param verbose: Verbosity level: 0 for no output, 1 for info messages, 2 for debug messages
+    """
+    def __init__(self, verbose: int = 0):
+        super().__init__(verbose)
+        # Those variables will be accessible in the callback
+        # (they are defined in the base class)
+        # The RL model
+        # self.model = None  # type: BaseAlgorithm
+        # An alias for self.model.get_env(), the environment used for training
+        # self.training_env # type: VecEnv
+        # Number of time the callback was called
+        # self.n_calls = 0  # type: int
+        # num_timesteps = n_envs * n times env.step() was called
+        # self.num_timesteps = 0  # type: int
+        # local and global variables
+        # self.locals = {}  # type: Dict[str, Any]
+        # self.globals = {}  # type: Dict[str, Any]
+        # The logger object, used to report things in the terminal
+        # self.logger # type: stable_baselines3.common.logger.Logger
+        # Sometimes, for event callback, it is useful
+        # to have access to the parent object
+        # self.parent = None  # type: Optional[BaseCallback]
+
+    def _on_training_start(self) -> None:
+        """
+        This method is called before the first rollout starts.
+        """
+        pass
+
+    def _on_rollout_start(self) -> None:
+        """
+        A rollout is the collection of environment interaction
+        using the current policy.
+        This event is triggered before collecting new samples.
+        """
+        pass
+
+    def _on_step(self) -> bool:
+        """
+        This method will be called by the model after each call to `env.step()`.
+
+        For child callback (of an `EventCallback`), this will be called
+        when the event is triggered.
+
+        :return: If the callback returns False, training is aborted early.
+        """
+        if self.n_calls % 1000 == 0:
+            if self.n_calls % 5000 == 0:
+                print(f"Updating")
+                import torch
+                weights = torch.load('ppo_f1tenth_10000/policy.pth')
+                self.model.policy.load_state_dict(weights)
+                print(self.model.policy.state_dict()['value_net.weight'])
+            else:
+                print(self.model.policy.state_dict()['value_net.weight'])
+        return True
+
+    def _on_rollout_end(self) -> None:
+        """
+        This event is triggered before updating the policy.
+        """
+        pass
+
+    def _on_training_end(self) -> None:
+        """
+        This event is triggered before exiting the `learn()` method.
+        """
+        pass
+
 """
 Planner Helpers
 """
@@ -311,8 +387,12 @@ def main():
 
     # SB3
     from stable_baselines3 import PPO
-    model = PPO('MlpPolicy', env, verbose=1, tensorboard_log="./ppo_f1tenth_tensorboard/ppo1")
-    model.learn(total_timesteps=1e7)
+    model = PPO('MlpPolicy', env, verbose=1)
+    model.learn(total_timesteps=10000, callback=CustomCallback())
+
+
+
+
 
 
 if __name__ == '__main__':
