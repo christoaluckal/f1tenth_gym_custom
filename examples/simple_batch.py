1fefb41c5102e745b4b8e05c479f40d4d030caac
Christo Aluckal <christo12aluckal@gmail.com>
2024-03-24 02:12:37 +0530

    fixed 3 exp

--- examples/simple_batch.py
+++ examples/simple_batch.py
@@ -13,9 +13,9 @@ exp_str = " --ws True --ws_count 2 --save_freq " + str(sf) + " --modify_epoch " 
 
 exp1 = "python3 test_sb.py --config 1 --car_idx 1 --base 0 --retain 1" + exp_str  +" --exp 1 --verbose 1" 
 exp3 = "python3 test_sb.py --config 2 --car_idx 3 --base 0 --retain 0.8" + exp_str +" --exp 3 --verbose 1"
-# exp5 = "python3 test_sb.py --config 3 --car_idx 5 --base 0 --retain 0.6" + exp_str +" --exp 5 --verbose 1"
+exp5 = "python3 test_sb.py --config 3 --car_idx 5 --base 0 --retain 0.6" + exp_str +" --exp 5 --verbose 1"
 
-processes = [mp.Process(target=run_exp, args=(exp,)) for exp in [exp1,exp3]]
+processes = [mp.Process(target=run_exp, args=(exp,)) for exp in [exp1,exp3,exp5]]
 try:
     for p in processes:
         p.start()
