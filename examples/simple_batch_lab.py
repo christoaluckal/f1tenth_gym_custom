da051d8d1b8acc16caac6570c82f7ca601d20d6f
Christo Aluckal <christo12aluckal@gmail.com>
2024-03-24 02:30:01 +0530

    spacing

--- examples/simple_batch_lab.py
+++ examples/simple_batch_lab.py
@@ -14,8 +14,8 @@ exp_str = " --is_lab True --ws True --ws_count 3 --save_freq " + str(sf) + " --m
 
 for r in ratios:
     exp1 = "python3 test_sb.py --config 1 --car_idx 1 --base 0 --retain " + str(r[0]) + exp_str  +" --exp 1 --verbose 1" 
-    exp3 = "python3 test_sb.py --config 2 --car_idx 3 --base 0 --retain 0.8" + str(r[1]) + exp_str +" --exp 3 --verbose 1"
-    exp5 = "python3 test_sb.py --config 3 --car_idx 5 --base 0 --retain 0.6" + str(r[2]) + exp_str +" --exp 5 --verbose 1"
+    exp3 = "python3 test_sb.py --config 2 --car_idx 3 --base 0 --retain " + str(r[1]) + exp_str +" --exp 3 --verbose 1"
+    exp5 = "python3 test_sb.py --config 3 --car_idx 5 --base 0 --retain " + str(r[2]) + exp_str +" --exp 5 --verbose 1"
 
     processes = [mp.Process(target=run_exp, args=(exp,)) for exp in [exp1,exp3,exp5]]
     try:
