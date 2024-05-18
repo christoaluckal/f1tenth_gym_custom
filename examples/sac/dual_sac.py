28a8599171853b4d358e40ac399826d9f302329c
Christo Aluckal <christo12aluckal@gmail.com>
2024-05-18 10:51:02 -0400

    changed params and added multi experiment runs

--- examples/sac/dual_sac.py
+++ examples/sac/dual_sac.py
@@ -46,19 +46,21 @@ kl_scales = zip(beta1s, beta2s)
 #     call(args="rm policy_*.pth",shell=True)
 
 # kl_scales = [0,1,5,10,0,30,50,0,100]
-kl_scales = [0,1,5,0,10,30,0,50,100,0]
 
-for idx,kl_scale in enumerate(kl_scales):
-    exp_1 = f"python3 main.py --own_policy_idx 1 --config 1 --env-name {exp_type}_1_sta_{idx}_{kl_scale} --cup_flag True --cuda --num_steps {ep} --kl_scale {kl_scale} --total_configs {total_configs}"
-    exp_2 = f"python3 main.py --own_policy_idx 2 --config 2 --env-name {exp_type}_2_sta_{idx}_{kl_scale} --cup_flag True --cuda --num_steps {ep} --kl_scale {kl_scale} --total_configs {total_configs}"
-    exp_3 = f"python3 main.py --own_policy_idx 3 --config 3 --env-name {exp_type}_3_sta_{idx}_{kl_scale} --cup_flag True --cuda --num_steps {ep} --kl_scale {kl_scale} --total_configs {total_configs}"
+for _ in range(5):
+    kl_scales = [0,1,5,0,10,30,0,50,100,0]
 
-    processes = [mp.Process(target=run_exp, args=(exp,)) for exp in [exp_1, exp_2, exp_3]]
+    for idx,kl_scale in enumerate(kl_scales):
+        exp_1 = f"python3 main.py --own_policy_idx 1 --config 1 --env-name {exp_type}_1_sta_{idx}_{kl_scale} --cup_flag True --cuda --num_steps {ep} --kl_scale {kl_scale} --total_configs {total_configs}"
+        exp_2 = f"python3 main.py --own_policy_idx 2 --config 2 --env-name {exp_type}_2_sta_{idx}_{kl_scale} --cup_flag True --cuda --num_steps {ep} --kl_scale {kl_scale} --total_configs {total_configs}"
+        exp_3 = f"python3 main.py --own_policy_idx 3 --config 3 --env-name {exp_type}_3_sta_{idx}_{kl_scale} --cup_flag True --cuda --num_steps {ep} --kl_scale {kl_scale} --total_configs {total_configs}"
 
-    for p in processes:
-        p.start()
+        processes = [mp.Process(target=run_exp, args=(exp,)) for exp in [exp_1, exp_2, exp_3]]
 
-    for p in processes:
-        p.join()
+        for p in processes:
+            p.start()
 
-    call(args="rm policy_*.pth",shell=True)
+        for p in processes:
+            p.join()
+
+        call(args="rm policy_*.pth",shell=True)
