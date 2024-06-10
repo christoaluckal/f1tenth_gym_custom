dd51aa6294a28f769be64afa07d80da259e5d037
Christo Aluckal <christo12aluckal@gmail.com>
2024-06-09 21:29:07 -0400

    updated for multiple no scale experiments

--- examples/sac/dual_sac.py
+++ examples/sac/dual_sac.py
@@ -12,13 +12,13 @@ exp_type = "f110"
 ep = 500000
 total_configs = 3
 
-kl_scales = [1e-1,5e-1,1e-2,5e-2]
+kl_scales = [0,1,1e-1,1e-2]
 
-exp_base_1 = f"python3 main_multi.py --own_policy_idx 1 --config 1 --env-name {exp_type}_1_sta_{0}_{0} --cuda --num_steps {ep} --total_configs {total_configs} --kl_scale {0}"
+# exp_base_1 = f"python3 main_multi.py --own_policy_idx 1 --config 1 --env-name {exp_type}_1_sta_{0}_{0} --cuda --num_steps {ep} --total_configs {total_configs} --kl_scale {0}"
 exp_base_2 = f"python3 main_multi.py --own_policy_idx 2 --config 2 --env-name {exp_type}_2_sta_{0}_{0} --cuda --num_steps {ep} --total_configs {total_configs} --kl_scale {0}"
-exp_base_3 = f"python3 main_multi.py --own_policy_idx 3 --config 3 --env-name {exp_type}_3_sta_{0}_{0} --cuda --num_steps {ep} --total_configs {total_configs} --kl_scale {0}"
+# exp_base_3 = f"python3 main_multi.py --own_policy_idx 3 --config 3 --env-name {exp_type}_3_sta_{0}_{0} --cuda --num_steps {ep} --total_configs {total_configs} --kl_scale {0}"
 
-processes = [mp.Process(target=run_exp, args=(exp,)) for exp in [exp_base_1, exp_base_2, exp_base_3]]
+processes = [mp.Process(target=run_exp, args=(exp,)) for exp in [exp_base_2]]
 
 for p in processes:
     p.start()
